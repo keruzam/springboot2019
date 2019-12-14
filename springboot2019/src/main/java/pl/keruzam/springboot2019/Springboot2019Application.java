2621e5512eac90a3ba6152eb5166ae30e1a510a3
keruzam <mazurek.zg@gmail.com>
2019-12-14 23:21:22 +0100

    first upload

---
+++ springboot2019/src/main/java/pl/keruzam/springboot2019/Springboot2019Application.java
@@ -0,0 +1,13 @@
+package pl.keruzam.springboot2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2019Application {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(Springboot2019Application.class, args);
+	}
+
+}
---
+++ springboot2019/src/test/java/pl/keruzam/springboot2019/Springboot2019ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.keruzam.springboot2019;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2019ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
